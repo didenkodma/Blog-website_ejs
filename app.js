2b379afe38a1ccd4f3fbba5336a7a90da853e319
didenko.dma <didenko.dma@gmail.com>
2022-08-28 22:55:10 +0300

    Changes №3

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //jshint esversion:6
-
+//define main variables
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -9,56 +9,73 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 const posts = [];
-
 const app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+// home page
 app.get('/', (req, res) => {
+  let postsMin = [];
+  if (posts.length > 0) {
+
+    // posts.forEach((post) => {
+
+    //   let link = '/posts/' + _.trim(post.title);
+
+    //   let postMin = {
+    //     title : post.title,
+    //     body : post.body.slice(0, 100) + " ..." + ` <a href=${link}>Read More</a>`,
+    //   }
+    //   postsMin.push(postMin);
+    // })
+  }
+
   res.render('home', {
     homeStarting: homeStartingContent,
     posts: posts
   });
 });
 
+// about page
 app.get('/about', (req, res) => {
   res.render('about', {about: aboutContent});
 });
 
+// contact page
 app.get('/contact', (req, res) => {
   res.render('contact', {contact: contactContent});
 });
 
+// page compose
 app.get('/compose', (req, res) => {
   res.render('compose');
 });
 
+// page post
 app.get('/posts/:postName', (req, res) => {
 
   const postName = _.lowerCase(req.params.postName);
-  let currentPost;
+  let currentPostName, currentPost;
   
   posts.forEach((post) => {
 
     const storedName = post.title.toLowerCase();
 
     if (postName === storedName) {
-      console.log('Match found');
+      currentPostName = post.title;
       currentPost = post.body;
-    } else {
-
-    }
+    } 
 
   });
 
   res.render('post', {
-    postName: postName.toUpperCase(), 
+    postName: currentPostName, 
     currentPost: currentPost});
 });
 
+// page home post
 app.post('/', (req, res) => {
 
   const post = {
